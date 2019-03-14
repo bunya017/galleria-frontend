5596a167cdb11b141d7dfcde65803ddbf183f38d
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-03-14 23:14:10 +0100

    Add card to Signup.vue

--- src/pages/users/Signup.vue
+++ src/pages/users/Signup.vue
@@ -1,7 +1,18 @@
 <template>
   <q-page padding>
-    <!-- content -->
-    Signup
+    <div class="row justify-center">
+      <div class="col-12 col-sm-6 col-md-4">
+        <q-card>
+          <q-card-section>
+            <div class="text-h6 text-center">Sign up</div>
+            <div class="tesx-subtitle-2 text-center">
+              To create and manage your product catalogues.
+            </div>
+          </q-card-section>
+
+        </q-card>
+      </div>
+    </div>
   </q-page>
 </template>
 
