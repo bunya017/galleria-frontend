3c65093fa69c8d4b126284b6b8a57303fd675fff
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-03-15 08:22:45 +0100

    Add signup form to Signup.vue

--- src/pages/users/Signup.vue
+++ src/pages/users/Signup.vue
@@ -2,7 +2,7 @@
   <q-page padding>
     <div class="row justify-center">
       <div class="col-12 col-sm-6 col-md-4">
-        <q-card>
+        <q-card class="q-mt-lg" >
           <q-card-section>
             <div class="text-h6 text-center">Sign up</div>
             <div class="tesx-subtitle-2 text-center">
@@ -10,6 +10,42 @@
             </div>
           </q-card-section>
 
+          <q-card-section>
+            <div class="q-pa-md">
+              <form v-on:submit.prevent>
+                <div class="q-gutter-y-lg">
+                  <q-input outlined dense v-model="user.username" type="text" label="Username">
+                    <template v-slot:prepend>
+                      <q-icon name="person" />
+                    </template>
+                  </q-input>
+
+                  <q-input outlined dense v-model="user.email" type="email" label="Email">
+                    <template v-slot:prepend>
+                      <q-icon name="mail" />
+                    </template>
+                  </q-input>
+
+                  <q-input outlined dense v-model="user.password" :type="isPwd ? 'password' : 'text'" label="Password">
+                    <template v-slot:prepend>
+                      <q-icon name="vpn_key" />
+                    </template>
+                    <template v-slot:append>
+                      <q-icon
+                        :name="isPwd ? 'visibility_off' : 'visibility'"
+                        class="cursor-pointer"
+                        @click="isPwd = !isPwd"
+                      />
+                    </template>
+                  </q-input>
+
+                  <div class="col-6">
+                    <q-btn flat class="full-width bg-primary" type="submit" text-color="white" color="primary" label="Sign up" />
+                  </div>
+                </div>
+              </form>
+            </div>
+          </q-card-section>
         </q-card>
       </div>
     </div>
@@ -19,6 +55,12 @@
 <script>
 export default {
   // name: 'PageName',
+  data: function () {
+    return {
+      isPwd: true,
+      user: {}
+    }
+  }
 }
 </script>
 
