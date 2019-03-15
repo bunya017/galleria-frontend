491cbb5ceaf6fe1f37a10a02b09c461ae3ecf4b8
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-03-15 21:12:47 +0100

    Polish DashboardLayout setup

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -2,72 +2,31 @@
   <q-layout view="hHh Lpr fFf"> <!-- Be sure to play with the Layout demo on docs -->
 
     <!-- (Optional) The Header -->
-    <q-header elevated>
+    <q-header elevated class="bg-white text-primary">
       <q-toolbar>
+        <q-toolbar-title>
+          Galleria
+        </q-toolbar-title>
         <q-btn
           flat
           round
           dense
           icon="menu"
+          class="lt-md"
           @click="leftDrawer = !leftDrawer"
         />
-        <q-toolbar-title>
-          Header
-        </q-toolbar-title>
       </q-toolbar>
-
-      <q-tabs>
-        <q-route-tab
-          icon="map"
-          to="/your/route"
-          replace
-          label="One Tab"
-        />
-        <q-route-tab
-          icon="assignment"
-          to="/some/other/route"
-          replace
-          label="Other Tab"
-        />
-      </q-tabs>
     </q-header>
-
-    <!-- (Optional) The Footer -->
-    <q-footer>
-      <q-tabs top-indicator>
-        <q-route-tab
-          icon="map"
-          to="/your/route"
-          replace
-          label="One Tab"
-        />
-        <q-route-tab
-          icon="assignment"
-          to="/some/other/route"
-          replace
-          label="Other Tab"
-        />
-      </q-tabs>
-
-      <q-toolbar>
-        <q-btn
-          flat
-          round
-          dense
-          icon="menu"
-          @click="leftDrawer = !leftDrawer"
-        />
-        <q-toolbar-title>
-          Footer
-        </q-toolbar-title>
-      </q-toolbar>
-    </q-footer>
 
     <!-- (Optional) A Drawer; you can add one more with side="right" or change this one's side -->
     <q-drawer
       v-model="leftDrawer"
       side="left"
       bordered
+      show-if-above
+      no-swipe-open
+      no-swipe-close
+      :width="250"
       content-class="bg-grey-2"
     >
       <!-- QScrollArea is optional -->
