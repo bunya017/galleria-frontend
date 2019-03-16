2362e0ead6bba59fd148bd8321439ac7a650c526
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-03-16 15:38:13 +0100

    Add getCatalogDetail method

--- src/pages/catalogs/CatalogDetail.vue
+++ src/pages/catalogs/CatalogDetail.vue
@@ -1,12 +1,11 @@
 <template>
   <q-page padding>
-    <div class="text-h4">My Catalog</div>
+    <div class="text-h4">{{ catalog.name }}</div>
     <div class="q-px-sm q-gutter-sm">
-      <q-breadcrumbs>
+      <q-breadcrumbs separator=">">
         <q-breadcrumbs-el label="Dashboard" :to="{name:'my-catalogs'}" />
         <q-breadcrumbs-el
-          label="Dashboard"
-          separator=">"
+          :label="catalog.name"
           :to="{
             name:'my-catalogs',
             params: {
@@ -30,7 +29,22 @@ export default {
   methods: {
     getAuthToken: function () {
       return sessionStorage.getItem('authToken')
+    },
+    getCatalogDetail: function () {
+      let self = this
+      this.$axios.defaults.headers.common = {
+        'Authorization': 'Token ' + self.getAuthToken()
+      }
+      self.$axios.get(
+        'catalogs/' + self.$route.params.slug
+      )
+        .then(function (response) {
+          self.catalog = response.data
+        })
     }
+  },
+  created: function () {
+    this.getCatalogDetail()
   }
 }
 </script>
