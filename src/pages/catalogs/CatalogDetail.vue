9589bc6871028f6f9c2802d06ea86dc89749120b
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-03-16 14:30:01 +0100

    Add header and breadcrumbs to catalogDetail

--- src/pages/catalogs/CatalogDetail.vue
+++ src/pages/catalogs/CatalogDetail.vue
@@ -1,6 +1,21 @@
 <template>
   <q-page padding>
-    Detail
+    <div class="text-h4">My Catalog</div>
+    <div class="q-px-sm q-gutter-sm">
+      <q-breadcrumbs>
+        <q-breadcrumbs-el label="Dashboard" :to="{name:'my-catalogs'}" />
+        <q-breadcrumbs-el
+          label="Dashboard"
+          separator=">"
+          :to="{
+            name:'my-catalogs',
+            params: {
+              slug: this.$route.params.slug
+            }
+          }"
+        />
+      </q-breadcrumbs>
+    </div>
   </q-page>
 </template>
 
