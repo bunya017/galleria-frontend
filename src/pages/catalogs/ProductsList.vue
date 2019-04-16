1e2f3e73a985fdc58cf4d68502ff199a88d619a6
Bunyaminu Sani <dollabills007@gmail.com>
2019-04-16 14:04:25 +0100

    add getCatalogdetail method

--- src/pages/catalogs/ProductsList.vue
+++ src/pages/catalogs/ProductsList.vue
@@ -1,12 +1,30 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <div class="text-h4">Products List</div>
   </q-page>
 </template>
 
 <script>
 export default {
-  // name: 'PageName',
+  data: function () {
+    return {
+      catalog: {}
+    }
+  },
+  methods: {
+    getCatalogDetail: function () {
+      let self = this
+      this.$axios.defaults.headers.common = {
+        'Authorization': 'Token ' + self.getAuthToken()
+      }
+      self.$axios.get(
+        'catalogs/' + self.$route.params.slug
+      )
+        .then(function (response) {
+          self.catalog = response.data
+        })
+    }
+  }
 }
 </script>
 
