3139dd25dc501e402bb9d83d9fbe573f521f012b
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-05-17 12:30:00 +0100

    Add name of product to be deleted to confirm dialog.

--- src/pages/catalogs/ProductsList.vue
+++ src/pages/catalogs/ProductsList.vue
@@ -247,7 +247,7 @@
       <q-card>
         <q-card-section class="row items-center">
           <q-avatar icon="warning" color="white" text-color="negative" />
-          <span class="q-ml-md">Are you sure you want to delete this?</span>
+          <span class="q-ml-md">Are you sure you want to delete <span class="text-weight-bold">{{ deleteProductPayload.name }}</span> permanently?</span>
         </q-card-section>
         <q-card-actions align="right">
           <q-btn flat label="Cancel" color="primary" v-close-popup />
