00dbd31d088e8d2c0169549dc36c65867be48bf6
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-05-22 13:52:54 +0100

    Remove image uploader from div wrapper.

--- src/pages/catalogs/ProductsList.vue
+++ src/pages/catalogs/ProductsList.vue
@@ -92,76 +92,74 @@
                 </template>
               </q-input>
               <!-- Image uploader -->
-              <div class="row">
-                <q-uploader
-                  class="col"
-                  ref="photoFiles"
-                  label="Photos"
-                  color="white"
-                  text-color="grey-8"
-                  accept=".png, .jpeg, .jpg, .gif"
-                  multiple
-                  hide-upload-btn
-                >
-                  <template v-slot:header="scope">
-                    <div class="row no-wrap items-center q-pa-sm q-gutter-xs" :class="{ 'negative-border': photosError.status }">
-                      <q-btn
-                        v-if="scope.queuedFiles.length > 0"
-                        icon="clear_all" @click="scope.removeQueuedFiles"
-                        round
-                        dense
-                        flat
-                      />
-                      <div class="col">
-                        <div class="q-uploader__title" :class="{ 'text-negative': photosError.status }">Photos</div>
-                        <div class="q-uploader__subtitle" v-if="scope.queuedFiles.length > 0">{{ scope.uploadSizeLabel }}</div>
-                        <div class="q-uploader__subtitle text-negative" v-if="photosError.status === true">This field is required.</div>
-                      </div>
-                      <q-btn
-                        v-if="photosError.status === true"
-                        icon="error"
-                        color="negative"
-                        round
-                        dense
-                        flat
-                      />
-                      <q-btn v-if="scope.canAddFiles" icon="add_box" round dense flat>
-                        <q-uploader-add-trigger />
-                      </q-btn>
+              <q-uploader
+                class="col"
+                ref="photoFiles"
+                label="Photos"
+                color="white"
+                text-color="grey-8"
+                accept=".png, .jpeg, .jpg, .gif"
+                multiple
+                hide-upload-btn
+              >
+                <template v-slot:header="scope">
+                  <div class="row no-wrap items-center q-pa-sm q-gutter-xs" :class="{ 'negative-border': photosError.status }">
+                    <q-btn
+                      v-if="scope.queuedFiles.length > 0"
+                      icon="clear_all" @click="scope.removeQueuedFiles"
+                      round
+                      dense
+                      flat
+                    />
+                    <div class="col">
+                      <div class="q-uploader__title" :class="{ 'text-negative': photosError.status }">Photos</div>
+                      <div class="q-uploader__subtitle" v-if="scope.queuedFiles.length > 0">{{ scope.uploadSizeLabel }}</div>
+                      <div class="q-uploader__subtitle text-negative" v-if="photosError.status === true">This field is required.</div>
                     </div>
-                  </template>
-                  <template v-slot:list="scope">
-                    <q-list separator>
-                      <q-item v-for="file in scope.files" :key="file.name">
-                        <q-item-section>
-                          <q-item-label class="full-width ellipsis">
-                            {{ file.name }}
-                          </q-item-label>
-                          <q-item-label caption>
-                            {{ file.__sizeLabel }}
-                          </q-item-label>
-                        </q-item-section>
-                        <q-item-section
-                          v-if="file.__img"
-                          thumbnail
-                        >
-                          <img :src="file.__img.src" class="product-photo">
-                        </q-item-section>
-                        <q-item-section top side>
-                          <q-btn
-                            size="12px"
-                            flat
-                            dense
-                            round
-                            icon="delete"
-                            @click="scope.removeFile(file)"
-                          />
-                        </q-item-section>
-                      </q-item>
-                    </q-list>
-                  </template>
-                </q-uploader>
-              </div>
+                    <q-btn
+                      v-if="photosError.status === true"
+                      icon="error"
+                      color="negative"
+                      round
+                      dense
+                      flat
+                    />
+                    <q-btn v-if="scope.canAddFiles" icon="add_box" round dense flat>
+                      <q-uploader-add-trigger />
+                    </q-btn>
+                  </div>
+                </template>
+                <template v-slot:list="scope">
+                  <q-list separator>
+                    <q-item v-for="file in scope.files" :key="file.name">
+                      <q-item-section>
+                        <q-item-label class="full-width ellipsis">
+                          {{ file.name }}
+                        </q-item-label>
+                        <q-item-label caption>
+                          {{ file.__sizeLabel }}
+                        </q-item-label>
+                      </q-item-section>
+                      <q-item-section
+                        v-if="file.__img"
+                        thumbnail
+                      >
+                        <img :src="file.__img.src" class="product-photo">
+                      </q-item-section>
+                      <q-item-section top side>
+                        <q-btn
+                          size="12px"
+                          flat
+                          dense
+                          round
+                          icon="delete"
+                          @click="scope.removeFile(file)"
+                        />
+                      </q-item-section>
+                    </q-item>
+                  </q-list>
+                </template>
+              </q-uploader>
               <q-card-actions align="right" class="q-gutter-x-md q-pt-lg">
                 <q-btn flat label="Cancel" color="negative" v-close-popup />
                 <q-btn flat type="submit" label="Add new" color="primary" />
