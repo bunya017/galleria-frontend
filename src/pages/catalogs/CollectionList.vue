bb63d5791de15c0f754ee8690428c849b714d443
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-07-11 16:55:45 +0100

    refactor(CollectionList): Remove QTable for listing collections and use QList instead

--- src/pages/catalogs/CollectionList.vue
+++ src/pages/catalogs/CollectionList.vue
@@ -1,43 +1,26 @@
 <template>
   <q-page padding>
-    <!-- Collectins List -->
-    <div class="row q-at-sm q-pb-xl">
-      <div class="col-12">
-        <q-table
-          :data="collections"
-          :columns="columns"
-          row-key="name"
-          flat
-          hide-header
-          hide-bottom
-        >
-          <template v-slot:top>
-            <div class="text-h4">Collections</div>
-            <q-space />
-            <q-btn
-              class="bg-primary q-mt-xs-md"
-              flat
-              color="white"
-              icon="add"
-              label="new collection"
-            />
-          </template>
-          <template v-slot:body-cell-name="props">
-            <q-td :props="props">
-              <q-item class="q-pa-none">
-                <q-item-section side>
-                  <q-avatar color="primary" text-color="white" size="56px">
-                    {{ props.row.name.charAt(0).toUpperCase() }}
-                  </q-avatar>
-                </q-item-section>
-                <q-item-section>
-                  <q-item-label>{{ props.row.name }}</q-item-label>
-                  <q-item-label caption lines="1">{{ props.row.description }}</q-item-label>
-                </q-item-section>
-              </q-item>
-            </q-td>
-          </template>
-        </q-table>
+    <!-- Collections List -->
+    <div class="row q-pt-sm q-pb-xl q-col-gutter-md">
+      <div class="col-12" v-for="collection in collections" :key="collection.name">
+        <q-card>
+          <q-list>
+            <q-item>
+              <q-item-section avatar>
+                <q-avatar color="primary" text-color="white" size="56px">
+                  {{ collection.name.charAt(0).toUpperCase() }}
+                </q-avatar>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>{{ collection.name }}</q-item-label>
+                <q-item-label caption>{{ collection.description }}</q-item-label>
+              </q-item-section>
+              <q-item-section side>
+                <q-btn size="12px" flat dense round icon="more_vert" />
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-card>
       </div>
     </div>
   </q-page>
@@ -48,11 +31,7 @@ export default {
   name: 'CollectionList',
   data: function () {
     return {
-      collections: [],
-      columns: [
-        { name: 'name', label: 'COLLECTIONS', field: 'name', align: 'left' },
-        { name: 'actions', label: 'ACTIONS', align: 'left' }
-      ]
+      collections: []
     }
   },
   methods: {
