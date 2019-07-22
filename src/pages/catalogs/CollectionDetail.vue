9dc245c04cc0ef8820bd3225dfe13fb9ca60544f
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-07-22 17:49:58 +0100

    style(CollectionDetail): Change button color

--- src/pages/catalogs/CollectionDetail.vue
+++ src/pages/catalogs/CollectionDetail.vue
@@ -162,7 +162,7 @@
                 </template>
               </q-select>
               <q-card-actions align="right" class="q-gutter-x-md q-pt-lg">
-                <q-btn flat label="Cancel" color="negative" v-close-popup />
+                <q-btn flat label="Cancel" color="grey-7" v-close-popup />
                 <q-btn flat type="submit" label="Add new" color="primary" />
               </q-card-actions>
             </form>
