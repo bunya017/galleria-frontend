357c56a08dc368ff49c060ba60899e2b323f8b4b
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-07-30 11:19:24 +0100

    refactor(store.sidebar) Remove getSidebarCatalogs() from getters

--- src/store/modules/sidebar.module.js
+++ src/store/modules/sidebar.module.js
@@ -7,10 +7,6 @@ export const sidebarModule = {
       state.sidebarCatalogs = catalogList
     }
   },
-  getters: {
-    getSidebarCatalogs (state) {
-      return state.sidebarCatalogs
-    }
-  },
+  getters: {},
   actions: {}
 }
