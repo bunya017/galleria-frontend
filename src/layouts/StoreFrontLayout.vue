cbfc70057fcbee0d990fbe80b6260a7ebf3be288
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-02 12:55:06 +0100

    refactor(StoreFrontLayout): Add StoreFrontLayout.vue

---
+++ src/layouts/StoreFrontLayout.vue
@@ -0,0 +1,54 @@
+<template>
+  <q-layout view="hHh Lpr fFf"> <!-- Be sure to play with the Layout demo on docs -->
+
+    <!-- (Optional) The Header -->
+    <q-header elevated class="bg-white text-primary">
+      <q-toolbar>
+        <q-btn
+          flat
+          round
+          dense
+          icon="menu"
+          @click="leftDrawer = !leftDrawer"
+        />
+
+        <q-toolbar-title>
+          Galleria
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawer"
+      side="left"
+      bordered
+      show-if-above
+      no-swipe-open
+      no-swipe-close
+      :width="250"
+      content-class="bg-grey-2"
+    >
+      <!-- QScrollArea is optional -->
+      <q-scroll-area class="fit q-pa-sm">
+        <!-- Content here -->
+      </q-scroll-area>
+    </q-drawer>
+
+    <q-page-container>
+      <!-- This is where pages get injected -->
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'StoreFrontLayout',
+  data () {
+    return {
+      leftDrawer: true
+    }
+  }
+}
+</script>
