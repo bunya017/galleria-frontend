817fba3cc6df7c44a2247dda0c597ab223f77d49
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-02 23:25:31 +0100

    refactor(StoreHome): Wrap collections & categories card in div

--- src/pages/storeFront/StoreHome.vue
+++ src/pages/storeFront/StoreHome.vue
@@ -1,17 +1,32 @@
 <template>
   <q-page padding>
     <!-- content -->
-    <div class="row">
-      <div class="col-6">
-        <q-card style="width: 100%">
-          <q-img
-            :src="catalog.categories[0].background_image.small"
-          >
-            <div class="absolute-full text-subtitle2 flex flex-center">
-              View by categories
-            </div>
-          </q-img>
-        </q-card>
+    <div class="row justify-center">
+      <div class="col-12 col-sm-10">
+        <div class="row q-col-gutter-sm">
+          <div class="col-6">
+            <q-card style="width: 100%">
+              <q-img
+                :src="catalog.categories[0].background_image.small"
+              >
+                <div class="absolute-full text-subtitle2 flex flex-center">
+                  Categories
+                </div>
+              </q-img>
+            </q-card>
+          </div>
+          <div class="col-6">
+            <q-card style="width: 100%">
+              <q-img
+                :src="catalog.collections[0].background_image.small"
+              >
+                <div class="absolute-full text-subtitle2 flex flex-center">
+                  Collections
+                </div>
+              </q-img>
+            </q-card>
+          </div>
+        </div>
       </div>
     </div>
   </q-page>
