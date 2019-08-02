e1b5d7f492c296b6c895a658b4faa770d8fff550
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-02 20:51:51 +0100

    feat(StoreHome): Add StoreHome.vue

---
+++ src/pages/storeFront/StoreHome.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'StoreHome',
+  data () {
+    return {
+      products: []
+    }
+  }
+}
+</script>
+
+<style>
+</style>
