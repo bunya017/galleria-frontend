a71819579b098d081ca2599fe573d39a77a96fa8
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-02 16:02:06 +0100

    refactor(StoreProductList): Add products card

--- src/pages/storeFront/StoreProductList.vue
+++ src/pages/storeFront/StoreProductList.vue
@@ -1,6 +1,23 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <!-- Product List -->
+    <div class="row q-pt-lg q-pb-xl q-col-gutter-md">
+      <div
+        class="col-6 col-sm-4 col-md-3"
+        v-for="product in products"
+        :key="product.id"
+      >
+        <q-card>
+          <q-img
+            :src="product.photos[0].photo.small"
+            :ratio="1"
+          />
+          <q-card-section>
+            <div>{{ product.name }}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
   </q-page>
 </template>
 
