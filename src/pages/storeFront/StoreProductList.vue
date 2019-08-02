ab93f8921f41af425143dbd4692f163f75494abe
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-02 15:19:29 +0100

    refactor(StoreProductList): Add getProductList()

--- src/pages/storeFront/StoreProductList.vue
+++ src/pages/storeFront/StoreProductList.vue
@@ -9,6 +9,20 @@ export default {
   name: 'StoreProductList',
   data () {
     return {
+      products: []
+    }
+  },
+  methods: {
+    getProductsList: function () {
+      let self = this
+      self.$axios.get(
+        'catalogs/' + self.$route.params.catalogSlug + '/products/'
+      )
+        .then(function (response) {
+          if (response.status === 200) {
+            self.products = response.data
+          }
+        })
     }
   }
 }
