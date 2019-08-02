57d08d00c3fb3d38fe293f385219e87f5cf44306
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-02 17:25:30 +0100

    refactor(StoreProductList): Wrap product card in div.row

--- src/pages/storeFront/StoreProductList.vue
+++ src/pages/storeFront/StoreProductList.vue
@@ -1,21 +1,31 @@
 <template>
   <q-page padding>
     <!-- Product List -->
-    <div class="row q-pt-lg q-pb-xl q-col-gutter-md">
-      <div
-        class="col-6 col-sm-4 col-md-3"
-        v-for="product in products"
-        :key="product.id"
-      >
-        <q-card>
-          <q-img
-            :src="product.photos[0].photo.small"
-            :ratio="1"
-          />
-          <q-card-section>
-            <div>{{ product.name }}</div>
-          </q-card-section>
-        </q-card>
+    <div class="row justify-center">
+      <div class="col-12 col-sm-10">
+        <div class="row q-pt-lg q-pb-xl q-col-gutter-md">
+          <div
+            class="col-6 col-sm-4 col-md-3"
+            v-for="product in products"
+            :key="product.id"
+          >
+            <q-card flat>
+              <q-img
+                :src="product.photos[0].photo.small"
+                :ratio="1"
+              />
+              <q-card-section class="q-pl-none q-pt-xs">
+                <div class="text-subtitle2 text-uppercase">
+                  {{ product.name }}
+                </div>
+                <div class="text-grey-6 q-pa-none text-caption">
+                  {{ product.category.name }}
+                </div>
+                <div class="text-subtitle2">₦{{ product.price }}</div>
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
       </div>
     </div>
   </q-page>
