5e41fbd801bccfa7d1671bbf2cf24a85ab4ab561
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-02 14:53:06 +0100

    refactor(StoreProductList): Add StoreProductList.vue

---
+++ src/pages/storeFront/StoreProductList.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+    StoreProducts
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'StoreProductList',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
