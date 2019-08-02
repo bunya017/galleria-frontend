f5eb8b24d8f26ee7bc2ec4770f6c40ec5ebfd9e4
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-02 22:03:11 +0100

    refactor(StoreFrontLayout): Remove QDrawer and drawer button

--- src/layouts/StoreFrontLayout.vue
+++ src/layouts/StoreFrontLayout.vue
@@ -4,36 +4,11 @@
     <!-- (Optional) The Header -->
     <q-header elevated class="bg-white text-primary">
       <q-toolbar>
-        <q-btn
-          flat
-          round
-          dense
-          icon="menu"
-          @click="leftDrawer = !leftDrawer"
-        />
-
         <q-toolbar-title>
           Galleria
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
-
-    <q-drawer
-      v-model="leftDrawer"
-      side="left"
-      bordered
-      show-if-above
-      no-swipe-open
-      no-swipe-close
-      :width="250"
-      content-class="bg-grey-2"
-    >
-      <!-- QScrollArea is optional -->
-      <q-scroll-area class="fit q-pa-sm">
-        <!-- Content here -->
-      </q-scroll-area>
-    </q-drawer>
-
     <q-page-container>
       <!-- This is where pages get injected -->
       <router-view />
@@ -47,7 +22,6 @@ export default {
   name: 'StoreFrontLayout',
   data () {
     return {
-      leftDrawer: false
     }
   }
 }
