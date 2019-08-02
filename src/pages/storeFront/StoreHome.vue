4ec58a93b4320f1c4fe0ee8e0fb4c0c053d055fc
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-02 22:17:34 +0100

    refactor(StoreHome): Add getCatalog()

--- src/pages/storeFront/StoreHome.vue
+++ src/pages/storeFront/StoreHome.vue
@@ -1,6 +1,7 @@
 <template>
   <q-page padding>
     <!-- content -->
+
   </q-page>
 </template>
 
@@ -9,7 +10,20 @@ export default {
   name: 'StoreHome',
   data () {
     return {
-      products: []
+      catalog: {}
+    }
+  },
+  methods: {
+    getCatalog: function () {
+      let self = this
+      self.$axios.get(
+        'catalogs/' + self.$route.params.catalogSlug + '/'
+      )
+        .then(function (response) {
+          if (response.status === 200) {
+            self.catalog = response.data
+          }
+        })
     }
   }
 }
