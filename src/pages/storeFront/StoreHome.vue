f6e371d21b04bd0b46a5a9afd8a4ad2786da5d37
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-25 20:09:49 +0100

    refactor(StoreHome): Add featured section

--- src/pages/storeFront/StoreHome.vue
+++ src/pages/storeFront/StoreHome.vue
@@ -5,7 +5,7 @@
       <!-- Catalog header -->
       <div class="col-12">
         <q-img
-          :src="catalog.categories[0].background_image.full_size"
+          :src="catalog.background_image.full_size"
           class="my-header-image"
         >
           <div class="absolute-full text-subtitle2 flex flex-center">
@@ -15,7 +15,7 @@
           </div>
         </q-img>
       </div>
-      <div class="col-12 col-sm-10 q-pa-sm q-pa-sm-md">
+      <div class="col-12 col-sm-11 col-md-10 q-pa-sm q-pa-sm-md">
         <!-- Collections & Categories -->
         <div class="row q-col-gutter-sm" v-if="catalog">
           <div class="col-6">
@@ -46,6 +46,29 @@
               </q-img>
             </q-card>
           </div>
+        </div>
+
+        <!-- Featured products -->
+        <div class="my-text-head text-weight-thin text-left q-pt-lg">FEATURED</div>
+          <div class="row q-py-sm q-px-sm-md q-col-gutter-md no-wrap" style="overflow-x: auto;" v-if="catalog">
+            <div
+              v-for="product in products"
+              :key="product.id"
+              class="col-4 col-sm-3"
+            >
+              <q-card>
+                <q-img
+                  :src="product.photos[0].photo.small"
+                  :ratio="9/10"
+                />
+                <q-card-section class="q-pa-xs q-pa-sm-sm">
+                  <div class="text-capitalize text-sm-subtitle1 ellipsis">
+                    {{ product.name }}
+                  </div>
+                  <div class="text-grey-8 q-px-xs text-subtitle2 text-caption">₦{{ noDecimal(product.price) }}</div>
+                </q-card-section>
+              </q-card>
+            </div>
         </div>
       </div>
     </div>
@@ -83,6 +106,9 @@ export default {
             self.products = response.data
           }
         })
+    },
+    noDecimal: function (num) {
+      return Number(num).toFixed(0)
     }
   },
   created: function () {
