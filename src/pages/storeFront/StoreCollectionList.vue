819eecaa6640a7e55f2c78dead76e2046e1961c6
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-29 10:57:48 +0100

    chore: Add pages/storeFront/StoreCollectionList.vue

---
+++ src/pages/storeFront/StoreCollectionList.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'StoreCollectionList',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
