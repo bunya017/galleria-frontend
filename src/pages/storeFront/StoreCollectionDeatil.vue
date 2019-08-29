8e083af2e4173e9c269767bd2f8caef0c4086fd4
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-29 12:37:31 +0100

    chore: Add src/pages/storeFront/StoreCollectionDeatil.vue

---
+++ src/pages/storeFront/StoreCollectionDeatil.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'StoreCollectionDetail',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
