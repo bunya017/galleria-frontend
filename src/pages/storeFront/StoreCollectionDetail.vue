c91097b11e6b0da3e9c10ad2e898fe9476b2a063
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-08-29 12:45:23 +0100

    refactor(StoreCollDetail): Add getCollectionDetail()

--- src/pages/storeFront/StoreCollectionDetail.vue
+++ src/pages/storeFront/StoreCollectionDetail.vue
@@ -9,6 +9,22 @@ export default {
   name: 'StoreCollectionDetail',
   data () {
     return {
+      collection: {},
+      products: []
+    }
+  },
+  methods: {
+    getCollectionDetail: function () {
+      let self = this
+      self.$axios.get(
+        'catalogs/' + self.$route.params.catalogSlug + '/collections/' + self.$route.params.collectionSlug + '/'
+      )
+        .then(function (response) {
+          if (response.status === 200) {
+            self.collection = response.data
+            self.products = response.data.collection_products
+          }
+        })
     }
   }
 }
