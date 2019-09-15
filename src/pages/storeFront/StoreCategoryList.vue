0490ed3f4a3f5ac99618ae0a1792cba0e5008fd0
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-09-15 20:38:54 +0100

    refactor(StoreCatList): Add breadcrumbs

--- src/pages/storeFront/StoreCategoryList.vue
+++ src/pages/storeFront/StoreCategoryList.vue
@@ -1,5 +1,24 @@
 <template>
   <q-page padding>
+    <!-- Breadcrumbs -->
+    <div class="q-pa-sm q-gutter-sm">
+      <q-breadcrumbs
+        separator="/"
+        class="text-uppercase"
+      >
+        <q-breadcrumbs-el
+          label="Store"
+          :to="{
+            name: 'store-home',
+            params: {
+              slug: this.$route.params.catalogSlug
+            }
+          }"
+        />
+        <q-breadcrumbs-el label="Categories" />
+      </q-breadcrumbs>
+    </div>
+
   </q-page>
 </template>
 
