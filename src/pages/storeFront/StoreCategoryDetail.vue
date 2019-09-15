a69760f7f1b6c4e4f65ca6e41c349ded79a481b4
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-09-15 21:01:49 +0100

    refactor(StoreCatDetail): Add getCategoryDetail()

--- src/pages/storeFront/StoreCategoryDetail.vue
+++ src/pages/storeFront/StoreCategoryDetail.vue
@@ -5,6 +5,26 @@
 
 <script>
 export default {
-  name: 'StoreCategoryDetail'
+  name: 'StoreCategoryDetail',
+  data () {
+    return {
+      category: {},
+      categoryProducts: []
+    }
+  },
+  methods: {
+    getCategoryDetail: function () {
+      let self = this
+      self.$axios.get(
+        'catalogs/' + self.$route.params.catalogSlug + '/categories/' + self.$route.params.categorySlug + '/'
+      )
+        .then(function (response) {
+          if (response.status === 200) {
+            self.category = response.data
+            self.categoryProducts = response.data.category_products
+          }
+        })
+    }
+  }
 }
 </script>
