d1d3b115fe79c2695233945aa3dc05a31250fff9
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-09-15 20:32:25 +0100

    refactor(StoreCatList): Add getCategoryList()

---
+++ src/pages/storeFront/StoreCategoryList.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page padding>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'StorecategoryList',
+  data () {
+    return {
+      categories: []
+    }
+  },
+  methods: {
+    getCategoryList: function () {
+      let self = this
+      self.$axios.get(
+        'catalogs/' + self.$route.params.catalogSlug + '/categories/'
+      )
+        .then(function (response) {
+          if (response.status === 200) {
+            self.categories = response.data
+          }
+        })
+    }
+  }
+}
+</script>
