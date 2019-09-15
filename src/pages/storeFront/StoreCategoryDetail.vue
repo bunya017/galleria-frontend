7bbed11b327cf4ed7ecc83190b1b1142b01e41ab
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-09-15 21:15:54 +0100

    refactor(StoreCatDetail): Add page header section

--- src/pages/storeFront/StoreCategoryDetail.vue
+++ src/pages/storeFront/StoreCategoryDetail.vue
@@ -1,5 +1,19 @@
 <template>
   <q-page>
+    <!-- Category header -->
+    <div class="col-12">
+      <q-img
+        :src="category.background_image.full_size"
+        class="my-header-image"
+      >
+        <div class="absolute-full text-subtitle2 flex flex-center">
+          <!-- Header -->
+          <div class="text-h4 text-uppercase xs q-py-md">{{ category.name }}</div>
+          <div class="text-h3 text-uppercase sm q-py-md">{{ category.name }}</div>
+          <div class="text-h2 text-uppercase md q-py-md">{{ category.name }}</div>
+        </div>
+      </q-img>
+    </div>
   </q-page>
 </template>
 
@@ -31,3 +45,29 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  a {
+    text-decoration: none;
+  }
+  @media (max-width: 599px) {
+    .my-header-image {
+      height: 45vw;
+    }
+  }
+  @media (max-width: 860px) {
+    .my-header-image {
+      height: 35vw;
+    }
+  }
+  @media (max-width: 999px) {
+    .my-header-image {
+      height: 25vw;
+    }
+  }
+  @media (min-width: 1000px) {
+    .my-header-image {
+      height: 20vw;
+    }
+  }
+</style>
