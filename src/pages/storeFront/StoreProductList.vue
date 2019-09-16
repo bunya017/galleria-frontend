90b875772042cc3d9ab66fd1412812508ab94b55
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-09-16 20:04:58 +0100

    refactor(StoreProductList): Add header

--- src/pages/storeFront/StoreProductList.vue
+++ src/pages/storeFront/StoreProductList.vue
@@ -1,27 +1,32 @@
 <template>
   <q-page padding>
+    <!-- Header -->
+    <div class="text-h4 xs">All Products</div>
+    <div class="text-h3 sm">All Products</div>
+    <div class="text-h2 md">All Products</div>
+
     <!-- Product List -->
     <div class="row justify-center">
       <div class="col-12 col-sm-10">
-        <div class="row q-pt-lg q-pb-xl q-col-gutter-md">
+        <div class="row q-pt-sm q-pt-md-md q-pb-xl q-col-gutter-md">
           <div
             class="col-6 col-sm-4 col-md-3"
             v-for="product in products"
             :key="product.id"
           >
-            <q-card flat>
+            <q-card>
               <q-img
                 :src="product.photos[0].photo.small"
                 :ratio="1"
               />
-              <q-card-section class="q-pl-none q-pt-xs">
+              <q-card-section class="q-pa-xs text-center">
                 <div class="text-subtitle2 text-uppercase">
                   {{ product.name }}
                 </div>
                 <div class="text-grey-6 q-pa-none text-caption">
                   {{ product.category.name }}
                 </div>
-                <div class="text-subtitle2">₦{{ product.price }}</div>
+                <div class="text-grey-8">₦{{ product.price }}</div>
               </q-card-section>
             </q-card>
           </div>
