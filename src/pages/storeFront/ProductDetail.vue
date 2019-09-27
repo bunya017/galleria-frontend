635da5068802e609baf7a74eb2f54899c6dd95d1
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-09-27 14:30:57 +0100

    refactor(storeProdDetail): Add getProductDetail()

--- src/pages/storeFront/ProductDetail.vue
+++ src/pages/storeFront/ProductDetail.vue
@@ -9,6 +9,20 @@ export default {
   name: 'ProductDetail',
   data () {
     return {
+      product: {}
+    }
+  },
+  methods: {
+    getProductDetail: function () {
+      let self = this
+      self.$axios.get(
+        'catalogs/' + self.$route.params.catalogSlug + '/products/' + self.$route.params.productSlug + '/' + self.$route.params.referenceId
+      )
+        .then(function (response) {
+          if (response.status === 200) {
+            self.product = response.data
+          }
+        })
     }
   }
 }
