4e9a03dd0797c4d436f26af1f852b3afc6acf377
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-10-08 16:39:39 +0100

    refactor(StoreCatDetail): Add router-link to store-product-detail

--- src/pages/storeFront/StoreCategoryDetail.vue
+++ src/pages/storeFront/StoreCategoryDetail.vue
@@ -1,70 +1,84 @@
 <template>
   <q-page>
-    <!-- Category header -->
-    <div class="col-12">
-      <q-img
-        :src="category.background_image.full_size"
-        class="my-header-image"
-      >
-        <div class="absolute-full text-subtitle2 flex flex-center">
-          <!-- Header -->
-          <div class="text-h4 text-uppercase xs q-py-md">{{ category.name }}</div>
-          <div class="text-h3 text-uppercase sm q-py-md">{{ category.name }}</div>
-          <div class="text-h2 text-uppercase md q-py-md">{{ category.name }}</div>
-        </div>
-      </q-img>
-    </div>
+    <div v-if="category.background_image">
+      <!-- Category header -->
+      <div class="col-12">
+        <q-img
+          :src="category.background_image.full_size"
+          class="my-header-image"
+        >
+          <div class="absolute-full text-subtitle2 flex flex-center">
+            <!-- Header -->
+            <div class="text-h4 text-uppercase xs q-py-md">{{ category.name }}</div>
+            <div class="text-h3 text-uppercase sm q-py-md">{{ category.name }}</div>
+            <div class="text-h2 text-uppercase md q-py-md">{{ category.name }}</div>
+          </div>
+        </q-img>
+      </div>
 
-    <!-- Breadcrumbs -->
-    <div class="q-pa-sm q-pa-sm-md q-gutter-sm">
-      <q-breadcrumbs
-        separator="/"
-        class="text-uppercase"
-      >
-        <q-breadcrumbs-el
-          label="Store"
-          :to="{
-            name: 'store-home',
-            params: {
-              slug: this.$route.params.catalogSlug
-            }
-          }"
-        />
-        <q-breadcrumbs-el
-          label="Categories"
-          :to="{
-            name: 'store-category-list',
-            params: {
-              catalogSlug: this.$route.params.catalogSlug
-            }
-          }"
-        />
-        <q-breadcrumbs-el :label="category.name" />
-      </q-breadcrumbs>
-    </div>
+      <!-- Breadcrumbs -->
+      <div class="q-pa-sm q-pa-sm-md q-gutter-sm">
+        <q-breadcrumbs
+          separator="/"
+          class="text-uppercase"
+        >
+          <q-breadcrumbs-el
+            label="Store"
+            :to="{
+              name: 'store-home',
+              params: {
+                slug: this.$route.params.catalogSlug
+              }
+            }"
+          />
+          <q-breadcrumbs-el
+            label="Categories"
+            :to="{
+              name: 'store-category-list',
+              params: {
+                catalogSlug: this.$route.params.catalogSlug
+              }
+            }"
+          />
+          <q-breadcrumbs-el :label="category.name" />
+        </q-breadcrumbs>
+      </div>
 
-    <!-- Category products -->
-    <div class="q-px-sm q-px-sm-md">
-      <div class="row justify-center">
-        <div class="col-12 col-sm-10">
-          <div class="row q-pt-sm q-pt-md-md q-pb-xl q-col-gutter-md" v-if="products">
-            <div
-              class="col-6 col-sm-4 col-md-3"
-              v-for="product in products"
-              :key="product.id"
-            >
-              <q-card>
-                <q-img
-                  :src="product.photos[0].photo.small"
-                  :ratio="1"
-                />
-                <q-card-section class="q-pa-xs q-pa-sm-sm text-center">
-                  <div class="text-subtitle1 text-capitalize">
-                    {{ product.name }}
-                  </div>
-                  <div class="text-subtitle1 q-pt-sm-xs text-grey-8">₦{{ product.price }}</div>
-                </q-card-section>
-              </q-card>
+      <!-- Category products -->
+      <div class="q-px-sm q-px-sm-md">
+        <div class="row justify-center">
+          <div class="col-12 col-sm-10">
+            <div class="row q-pt-sm q-pt-md-md q-pb-xl q-col-gutter-md" v-if="products">
+              <div
+                class="col-6 col-sm-4 col-md-3"
+                v-for="product in products"
+                :key="product.id"
+              >
+                <router-link
+                  v-if="category.slug"
+                  :to="{
+                    name: 'store-product-detail',
+                    params: {
+                      catalogSlug: catalogSlug,
+                      referenceId: product.reference_id,
+                      productSlug: product.slug
+                    }
+                  }"
+                >
+                  <q-card>
+                    <q-img
+                      :src="product.photos[0].photo.small"
+                      :ratio="1"
+                    />
+                    <q-card-section class="q-pa-xs q-pa-sm-sm text-center text-black">
+                      <div class="text-subtitle1 text-capitalize">
+                        {{ product.name }}
+                      </div>
+                      <div class="text-subtitle1 q-pt-sm-xs text-grey-8">₦{{ product.price }}</div>
+                    </q-card-section>
+                  </q-card>
+                </router-link>
+              </div>
             </div>
           </div>
         </div>
@@ -78,6 +92,7 @@ export default {
   name: 'StoreCategoryDetail',
   data () {
     return {
+      catalogSlug: this.$route.params.catalogSlug,
       category: {},
       products: []
     }
