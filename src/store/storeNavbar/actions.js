4cdbebf8f0b1ea5a038e1a550812f66241c89fa9
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-10-09 22:22:57 +0100

    refactor(store.navbar): Add updateCatalogAction action

--- src/store/storeNavbar/actions.js
+++ src/store/storeNavbar/actions.js
@@ -1,4 +1,12 @@
-/*
-export function someAction (context) {
+import axios from 'axios'
+
+export function updateCatalogAction ({ commit }, catalogSlug) {
+  return axios.get(
+    'http://localhost:8000/api/catalogs/' + catalogSlug + '/'
+  )
+    .then(function (response) {
+      if (response.status === 200) {
+        return commit('updateCatalog', { name: response.data.name, slug: response.data.slug })
+      }
+    })
 }
-*/
