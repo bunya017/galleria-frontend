56dfd402403d1368da532190b09ab8a5da8a9217
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-10-10 12:40:35 +0100

    fix(StoreCollectionList): Fix padding

--- src/pages/storeFront/StoreCollectionList.vue
+++ src/pages/storeFront/StoreCollectionList.vue
@@ -1,58 +1,60 @@
 <template>
-  <q-page padding>
-    <!-- Header -->
-    <div class="text-h4 xs">Collections</div>
-    <div class="text-h3 sm">Collections</div>
-    <div class="text-h2 md">Collections</div>
+  <q-page>
+    <div class="q-px-md-xl q-py-md-sm q-pa-sm">
+      <!-- Header -->
+      <div class="text-h4 xs">Collections</div>
+      <div class="text-h3 sm">Collections</div>
+      <div class="text-h2 md">Collections</div>
 
-    <!-- Breadcrumbs -->
-    <div class="q-pa-sm q-pa-sm-md q-gutter-sm">
-      <q-breadcrumbs
-        separator="/"
-        class="text-uppercase"
-      >
-        <q-breadcrumbs-el
-          label="Store"
-          :to="{
-            name: 'store-home',
-            params: {
-              slug: this.$route.params.catalogSlug
-            }
-          }"
-        />
-        <q-breadcrumbs-el label="Collections" />
-      </q-breadcrumbs>
-    </div>
-
-    <!-- Collection List -->
-    <div class="row q-col-gutter-md q-pb-lg" v-if="collections">
-      <div
-        class="col-6 col-sm-4"
-        v-for="collection in collections"
-        :key="collection.id"
-      >
-        <router-link
-          :to="{
-            name: 'store-collection-detail',
-            params: {
-              catalogSlug: catalogSlug,
-              collectionSlug: collection.slug
-            }
-          }"
+      <!-- Breadcrumbs -->
+      <div class="q-pa-sm q-pa-sm-md q-gutter-sm">
+        <q-breadcrumbs
+          separator="/"
+          class="text-uppercase"
         >
-          <q-card style="width: 100%">
-            <q-img
-              :src="collection.background_image.small"
-              class="my-card-image"
-            >
-              <div class="absolute-full flex flex-center">
-                <div class="my-text-head text-subtitle2 text-capitalize text-weight-thin text-center">
-                  {{ collection.name }}
+          <q-breadcrumbs-el
+            label="Store"
+            :to="{
+              name: 'store-home',
+              params: {
+                slug: this.$route.params.catalogSlug
+              }
+            }"
+          />
+          <q-breadcrumbs-el label="Collections" />
+        </q-breadcrumbs>
+      </div>
+
+      <!-- Collection List -->
+      <div class="row q-col-gutter-md q-pb-lg" v-if="collections">
+        <div
+          class="col-6 col-sm-4"
+          v-for="collection in collections"
+          :key="collection.id"
+        >
+          <router-link
+            :to="{
+              name: 'store-collection-detail',
+              params: {
+                catalogSlug: catalogSlug,
+                collectionSlug: collection.slug
+              }
+            }"
+          >
+            <q-card style="width: 100%">
+              <q-img
+                :src="collection.background_image.small"
+                class="my-card-image"
+              >
+                <div class="absolute-full flex flex-center">
+                  <div class="my-text-head text-subtitle2 text-capitalize text-weight-thin text-center">
+                    {{ collection.name }}
+                  </div>
                 </div>
-              </div>
-            </q-img>
-          </q-card>
-        </router-link>
+              </q-img>
+            </q-card>
+          </router-link>
+        </div>
       </div>
     </div>
   </q-page>
