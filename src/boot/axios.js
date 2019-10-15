9b43f9fd43513098d9247b1f38f6f43a25f9a13a
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-10-15 15:32:21 +0100

    refactor(boot/axios): Export axiosInstance

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,9 +1,10 @@
+import Vue from 'vue'
 import axios from 'axios'
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:8000/api'
 })
 
-export default async ({ Vue }) => {
-  Vue.prototype.$axios = axiosInstance
-}
+Vue.prototype.$axios = axiosInstance
+
+export { axiosInstance }
