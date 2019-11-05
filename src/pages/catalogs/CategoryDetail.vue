1234e948b162ebf1b429174aad055017c4ffd7c8
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-11-05 15:40:17 +0100

    feat(CatyDetail): Add category products list

--- src/pages/catalogs/CategoryDetail.vue
+++ src/pages/catalogs/CategoryDetail.vue
@@ -34,6 +34,47 @@
         {{ category.name }} Category
       </div>
     </div>
+
+    <!-- Category products list-->
+    <div class="row q-pt-sm q-col-gutter-md">
+      <div class="col-12" v-for="product in products" :key="product.id">
+        <q-card>
+          <q-list>
+            <q-item>
+              <q-item-section avatar>
+                <q-avatar v-if="product.photos.length > 0" rounded size="56px">
+                  <img :src="product.photos[0].photo.thumbnail">
+                </q-avatar>
+                <q-avatar v-else color="primary" text-color="white">
+                  {{ product.name.charAt(0).toUpperCase() }}
+                </q-avatar>
+              </q-item-section>
+              <q-item-section>
+                <router-link
+                  v-if="catalog.slug"
+                  :to="{
+                    name: 'product-detail',
+                    params: {
+                      catalogSlug: catalog.slug,
+                      referenceId: product.reference_id,
+                      productSlug: product.slug
+                    }
+                  }"
+                >
+                  <q-item-label class="text-black">{{ product.name }}</q-item-label>
+                  <q-item-label caption lines="2">
+                    {{ product.description }}
+                  </q-item-label>
+                  <q-item-label caption class="q-pt-sm text-weight-bold">
+                    ₦{{ product.price }}
+                  </q-item-label>
+                </router-link>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-card>
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -73,3 +114,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+</style>
