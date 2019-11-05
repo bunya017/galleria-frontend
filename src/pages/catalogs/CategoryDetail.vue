b69952aefd3183e736b7f678e703b8f6cd04d7cc
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-11-05 15:23:01 +0100

    refactor(CatyDetail): Add getCategoryDetail() to methods

--- src/pages/catalogs/CategoryDetail.vue
+++ src/pages/catalogs/CategoryDetail.vue
@@ -8,7 +8,32 @@
 export default {
   name: 'CategoryDetail',
   data () {
-    return {}
+    return {
+      catalog: {},
+      category: {},
+      products: []
+    }
+  },
+  methods: {
+    getAuthToken: function () {
+      return sessionStorage.getItem('authToken')
+    },
+    getCategoryDetail: function () {
+      let self = this
+      this.$axios.defaults.headers.common = {
+        'Authorization': 'Token ' + self.getAuthToken()
+      }
+      self.$axios.get(
+        'catalogs/' + self.$route.params.catalogSlug + '/categories/' + self.$route.params.categorySlug + '/'
+      )
+        .then(function (response) {
+          if (response.status === 200) {
+            self.category = response.data
+            self.catalog = response.data.catalog
+            self.products = response.data.product_entries
+          }
+        })
+    }
   }
 }
 </script>
