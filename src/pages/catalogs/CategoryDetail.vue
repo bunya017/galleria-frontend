b6c8573f7c82b4486191c72fc7ca713384296df5
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-11-05 15:03:34 +0100

    chore: Add src/pages/catalogs/CategoryDetail.vue

---
+++ src/pages/catalogs/CategoryDetail.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'CategoryDetail',
+  data () {
+  	return {}
+  }
+}
+</script>
