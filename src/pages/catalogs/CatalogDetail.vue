af6dcb38ae63bb8c602a24578ed0c69232a0a35c
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-11-05 15:48:30 +0100

    refactor(CatalogDetail): Add category bg_image as avatar

--- src/pages/catalogs/CatalogDetail.vue
+++ src/pages/catalogs/CatalogDetail.vue
@@ -132,7 +132,10 @@
           <q-list>
             <q-item>
               <q-item-section avatar>
-                <q-avatar color="primary" text-color="white">
+                <q-avatar v-if="category.background_image.thumbnail" size="56px">
+                  <img :src="category.background_image.thumbnail">
+                </q-avatar>
+                <q-avatar v-else color="primary" size="56px" text-color="white">
                   {{ getFirstLetters(category.name) }}
                 </q-avatar>
               </q-item-section>
