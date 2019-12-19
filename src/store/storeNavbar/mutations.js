462565b7855b60d36acf67f19ca1c486c892f171
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-12-19 12:05:35 +0100

    refactor(store.dashStore): Add contact info to updateCatalog mutation

--- src/store/storeNavbar/mutations.js
+++ src/store/storeNavbar/mutations.js
@@ -2,5 +2,8 @@ export function updateCatalog (state, payload) {
   state.catalog.name = payload.name
   state.catalog.slug = payload.slug
   state.catalog.logo = payload.logo
+  state.catalog.contactEmail = payload.contact_email
+  state.catalog.contactPhone = payload.contact_phone
+  state.catalog.contactAddress = payload.contact_address
   state.catalog.backgroundImage = payload.backgroundImage
 }
