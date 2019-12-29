df618efaaacbd550d536f3fc0dbaba4b86266a62
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2019-12-29 13:31:00 +0100

    refactor(Error404) Redesign Error404 page

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -1,22 +1,35 @@
-<template>
-  <div class="fixed-center text-center">
-    <p>
-      <img
-        src="~assets/sad.svg"
-        style="width:30vw;max-width:150px;"
-      >
-    </p>
-    <p class="text-faded">Sorry, nothing here...<strong>(404)</strong></p>
-    <q-btn
-      color="secondary"
-      style="width:200px;"
-      @click="$router.push('/')"
-    >Go back</q-btn>
+<template padding>
+  <div class="row jutify-center text-center" style="padding-top: 25vh;">
+    <div class="col-12 q-px-md">
+      <div class="text-h2 q-pb-lg">404</div>
+      <p class="text-body1">We can't seem to find the page you're looking for.</p>
+      <div class="q-gutter-sm q-py-sm">
+        <q-btn
+          color="primary"
+          label="Go back"
+          @click="handleBack"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Error404'
+  name: 'Error404',
+  methods: {
+    handleBack () {
+      if (this.loggedInStatus === true) {
+        return this.$router.back() || this.$router.push('my-catalogs')
+      } else {
+        return this.$router.back() || this.$router.push('/')
+      }
+    }
+  },
+  computed: {
+    loggedInStatus () {
+      return this.$store.state.dashStore.isLoggedIn
+    }
+  }
 }
 </script>
