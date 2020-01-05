fa7e635fb3a0d9c088e466cfef512b156db92c77
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2020-01-05 23:22:02 +0100

    feat(FeaturedProducts): Add getFeaturedProducts() method

--- src/pages/catalogs/FeaturedProducts.vue
+++ src/pages/catalogs/FeaturedProducts.vue
@@ -1,14 +1,66 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <!-- Title -->
+    <div class="text-h4 col-12 col-sm-6">
+      Featured Products
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
   name: 'FeaturedProducts',
+  meta () {
+    return {
+      title: 'Featured Products'
+    }
+  },
   data () {
-    return {}
+    return {
+      collection: {},
+      featuredProducts: [],
+      collectionNotFound: null
+    }
+  },
+  methods: {
+    getAuthToken: function () {
+      return sessionStorage.getItem('authToken')
+    },
+    getFeaturedProducts: function () {
+      let self = this
+      this.$q.loading.show({
+        spinnerColor: 'primary',
+        backgroundColor: 'white'
+      })
+      this.$axios.defaults.headers.common = {
+        'Authorization': 'Token ' + self.getAuthToken()
+      }
+      self.$axios.get(
+        'catalogs/' + self.catalogSlug + '/collections/featured-products/?is_featured=true'
+      )
+        .then(function (response) {
+          if (response.status === 200) {
+            self.collection = response.data
+            self.featuredProducts = response.data.collection_products
+            self.collectionNotFound = false
+            self.$q.loading.hide()
+          }
+        })
+        .catch(function (error) {
+          if (error.response.status === 404) {
+            self.collectionNotFound = true
+            self.$q.loading.hide()
+          }
+        })
+    }
+  },
+  computed: {
+    catalogSlug () {
+      return this.$route.params.catalogSlug
+    }
+  },
+  created () {
+    this.getFeaturedProducts()
   }
 }
 </script>
