59425e7b3cf9d55decd1750cd0c7706bcc744f8d
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2020-01-05 22:35:55 +0100

    chore: Add FeaturedProducts page

---
+++ src/pages/catalogs/FeaturedProducts.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'FeaturedProducts',
+  data () {
+    return {}
+  }
+}
+</script>
